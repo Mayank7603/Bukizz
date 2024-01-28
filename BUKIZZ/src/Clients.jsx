42848ec9a4d2f6dbcd2aebbd8b1edb3b0f12562f
Mayank7603 <94745992+Mayank7603@users.noreply.github.com>
2024-01-28 15:36:04 +0530

    Update Clients.jsx

--- BUKIZZ/src/Clients.jsx
+++ BUKIZZ/src/Clients.jsx
@@ -3,23 +3,28 @@ import choose from '../public/choose.png';
 export default function Clients() {
 	return (
 		<>
-			<div className="flex flex-col p-4 px-5 gap-3">
-				<div className="text-left font-bold text-3xl bg-gradient-to-r from-violet-600 to-sky-600 inline-block text-transparent bg-clip-text">
-					Schools that choose Bukizz
+			<div className="flex flex-col p-4 px-5 gap-3 md:flex-row md:justify-around ">
+				<div className="md:text-left md:w-[400px] md:ml-12">
+					<div className="text-left font-bold text-[35px]  bg-gradient-to-r from-violet-600 to-sky-600 inline-block text-transparent bg-clip-text md:pb-4">
+						Schools that we serve
+					</div>
+					<div className="flex flex-col gap-2 px-4 md:px-0 md:gap-4">
+						<p className="text-slate-500 text-left">
+							School authorities and businesses are our major clients. We’re
+							architects of a better digital future for them.
+						</p>
+						<p className="text-slate-500 text-left">
+							Craft your school's perfect solution.
+							<p className="inline text-black">
+								Talk to a Bukizz expert today!
+							</p>
+						</p>
+					</div>
+					<div className="mb-4 mx-2 flex justify-center items-center rounded-xl bg-gradient-to-r from-sky-500 via-blue-500 to-violet-600 text-white h-12 md:w-48 md:mt-8 md:px-0 font-semibold">
+						<button>Book Free Demo</button>
+					</div>
 				</div>
-				<div className="flex flex-col gap-2 px-4">
-					<p className="text-slate-400 text-left">
-						School authorities and businesses are our major clients. We’re
-						architects of a better digital future for them.
-					</p>
-					<p className="text-slate-400 text-left">
-						Craft your school's perfect solution. Talk to a Bukizz expert today!
-					</p>
-				</div>
-				<div className="mb-4 mx-2 flex justify-center items-center rounded-xl bg-gradient-to-r from-sky-400 via-blue-500 to-violet-600 text-white h-12">
-					<button>Book a Free Demo</button>
-				</div>
-				<div className="flex justify-center items-center ">
+				<div className="flex justify-center items-center md:mr-12">
 					<img
 						src={choose}
 						alt="Image"
