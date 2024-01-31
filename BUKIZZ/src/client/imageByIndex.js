c951458c623f48e64bc41f17a009cc9b1ff3105c
Mayank7603 <94745992+Mayank7603@users.noreply.github.com>
2024-02-01 01:31:00 +0530

    minor bug

--- BUKIZZ/src/client/imageByIndex.js
+++ BUKIZZ/src/client/imageByIndex.js
@@ -1,6 +1,6 @@
-import image1 from "../../public/carousel/property1.svg";
-import image2 from "../../public/carousel/property2.svg";
-import image3 from "../../public/carousel/property3.svg";
+import image1 from "../../public/carousel/Property1.svg";
+import image2 from "../../public/carousel/Property2.svg";
+import image3 from "../../public/carousel/Property3.svg";
 
 export const images = [
   image1, image2, image3
