eec89a3b38c0b653de3fa953ddf685ec7b826fa7
Mayank7603 <94745992+Mayank7603@users.noreply.github.com>
2024-02-05 09:51:22 +0530

    Update InstallBanner.jsx

--- BUKIZZ/src/installBanner/InstallBanner.jsx
+++ BUKIZZ/src/installBanner/InstallBanner.jsx
@@ -9,7 +9,7 @@ import './InstallBanner.css';
 
 function InstallBanner() {
 	return (
-		<section className=" mainBox backdrop-opacity-5 h-[520px] w-[85%] md:h-[255px]   items-center justify-center md:pt-0 rounded-2xl overflow-hidden flex flex-col md:flex-row mx-auto  ">
+		<section className=" mainBox backdrop-opacity-5 h-[520px] w-[85%] lg:h-[255px]   items-center justify-center lg:pt-0 rounded-2xl overflow-hidden flex flex-col lg:flex-row mx-auto  ">
 			<div className="w-auto h-full overflow-hidden  top-0 ">
 				<img
 					className="w-70 h-64 fixed top-0 left-[134px] r1 "
@@ -17,27 +17,27 @@ function InstallBanner() {
 					alt="Rect2"
 				/>
 				<img
-					className="fixed left-[0px] top-[3px] w-70 md:h-64 h-96 r1 "
+					className="fixed left-[0px] top-[3px] w-70 lg:h-64 h-96 r1 "
 					src={rect1}
 					alt="Rect1"
 				/>
 			</div>
 
-			<div className="flex flex-col justify-center  items-start gap-4 md:w-[90%] p-6 md:py-0 md:m-0 md:px-20 mr-16">
+			<div className="flex flex-col justify-center  items-start gap-4 lg:w-[90%] p-6 lg:py-0 lg:m-0 lg:px-20 mr-16">
 				<div className="font-bannertext text-[40px]  font-bold text-white text-left leading-snug">
 					One App for all your <br /> Student's Need
 				</div>
-				<div className="flex flex-col md:flex-row gap-4 relative w-full  items-start">
+				<div className="flex flex-col lg:flex-row gap-4 relative w-full  items-start">
 					<img
-						className="md:w-44 w-full h-full ml-5 md:m-0 md:h-[57px]"
+						className="lg:w-44 w-full h-full ml-5 lg:m-0 lg:h-[57px]"
 						src={apple}></img>
 					<img
-						className="md:w-44 h-full w-full ml-5 md:m-0  md:h-[57px]"
+						className="lg:w-44 h-full w-full ml-5 lg:m-0  lg:h-[57px]"
 						src={google}></img>
 				</div>
 			</div>
 
-			<div className=" w-[100] md:w-[50%] h-full md:ml-8 hidden md:inline-block ">
+			<div className=" w-[100] lg:w-[50%] h-full lg:ml-8 hidden lg:inline-block ">
 				<img
 					className="w-[16rem]  lg:w-[20rem] fixed top-0  right-24 z-[1]"
 					src={img1}></img>
@@ -46,8 +46,8 @@ function InstallBanner() {
 					src={img2}></img>
 			</div>
 
-			<div className="overflow-hiddenflex  justify-center items-center  md:hidden">
-				<div className="flex gap-4 justify-center w-[90%] md:w-full ml-4 md:m-0">
+			<div className="   justify-center items-center hidden lg:inline-block">
+				<div className="flex gap-4 justify-center w-[90%] lg:w-full ml-4 lg:m-0">
 					<img
 						className=""
 						src={mobile}></img>
