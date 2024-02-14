bdc841882e3efba7eb50ba899c23d301e80312d0
DineshSingh <pathaniadinesh05@gmail.com>
2024-02-14 15:44:10 +0530

    Changed the feature section to be a Carousel

--- BUKIZZ/src/featureSection/featureByIndex.js
+++ BUKIZZ/src/featureSection/featureByIndex.js
@@ -1,12 +1,10 @@
-import image1 from "../../public/feature/one.svg"
-import image2 from "../../public/feature/two.svg"
-import image3 from "../../public/feature/three.svg"
-import image4 from "../../public/feature/four.svg"
-import image5 from "../../public/feature/five.svg"
+import image1 from "../../public/feature/one.svg";
+import image2 from "../../public/feature/two.svg";
+import image3 from "../../public/feature/three.svg";
+import image4 from "../../public/feature/four.svg";
+import image5 from "../../public/feature/five.svg";
 
-export const images = [
-    image1, image2, image3, image4, image5
-];
+export const images = [image1, image2, image3, image4, image5];
 
 const imageByIndex = (index) => images[index % images.length];
 
--- BUKIZZ/tailwind.config.js
+++ BUKIZZ/tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        headline: ['headline', 'sans-serif'],
-        subtext: ['subtext', 'sans-serif'],
-        bannertext: ['bannertext', 'sans-serif'],
-        footerText: ['footerText', 'sans-serif'],
-        footerHeading: ['footerHeading', 'sans-serif']
-
-      }
+        headline: ["headline", "sans-serif"],
+        subtext: ["subtext", "sans-serif"],
+        bannertext: ["bannertext", "sans-serif"],
+        footerText: ["footerText", "sans-serif"],
+        footerHeading: ["footerHeading", "sans-serif"],
+      },
+      colors: {
+        mainBlue: " #39A7FFFC",
+      },
     },
   },
   plugins: [],
-}
+};
